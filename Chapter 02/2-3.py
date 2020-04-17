a31a4cb2e29e42f5ed3d352ce76f9836a09efb22
H43RO <h2is1234@naver.com>
2020-04-17 14:00:00 +0900

    2-3

--- Chapter 02/2-3.py
+++ Chapter 02/2-3.py
@@ -19,3 +19,46 @@ a = [1, 2, 3]
 b = [4, 5, 6]
 print(a + b)
 print(a * 3)
+print(len(a))
+
+print(str(a[2]) + " h i")
+a[2] = 5
+print(a)
+del a[1]
+print(a)
+a = [1, 2, 3, 4, 5]
+del a[:3]
+print(a)
+
+a = [1, 2, 3, 4, 5]
+a.append(4)
+print(a)
+a.sort()
+print(a)
+a = ['a', 'd', 'c']
+a.sort()
+print(a)
+a.reverse()
+print(a)
+a = [1, 2, 3]
+print(a.index(3))
+a = [1, 2, 3]
+a.insert(0, 4)
+print(a)
+a.insert(2, 5)
+print(a)
+a.remove(1)
+a.remove(2)
+print(a)
+
+a = [1, 2, 3, 4, 5, 1, 2, 3]
+a.remove(3)
+print(a)
+print(a.pop())
+print(a)
+
+print(a.pop(1))
+print(a)
+
+a = [1, 2, 3, 1]
+print(a.count(1))
